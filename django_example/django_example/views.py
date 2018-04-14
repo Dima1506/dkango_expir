72021c98f21dcedb3a3e7263e4ad8c2aaaba8396
Dmitry <ichyotckin@yandex.ru>
2018-04-14 17:41:27 +0300

    release_commit

--- django_example/django_example/views.py
+++ django_example/django_example/views.py
@@ -9,16 +9,23 @@ def birg_com(request):
         bir_p = resp2.json()['payload']['rates'][0]['buy']
         bir2_p = resp2.json()['payload']['rates'][1]['buy']
         resp = HttpResponse()
-        resp.write(str(bir_p)+"  "+str(bir2_p))
+        resp.write(str(bir_p)+"_"+str(bir2_p))
         resp.status_code = 200
     return resp
 
 def vk_com(request):
     if request.method == "GET":
+        token = '08e35f615f6579bda204ef012617ea8703349210c983a34068f0e87af95a371be9559cbe0ef0ad969358d'
         data_vk_vol_message = requests.get('https://api.vk.com/method/messages.getDialogs',
                              params={'v': 5.74, 'access_token': token}).json()['response']
+        data_vk_vol_friends = requests.get('https://api.vk.com/method/friends.getRequests',
+                    params={'v': 5.74, 'access_token': token}).json()['response']
+        count_unread = 0
+        for i in data_vk_vol_message['items']:
+            if 'unread' in i:
+                count_unread = count_unread + 1
         resp = HttpResponse()
-        resp.write(str(bir_p)+"  "+str(bir2_p))
+        resp.write(str(data_vk_vol_friends['count'])+"  "+str(count_unread))
         resp.status_code = 200
     return resp
 
